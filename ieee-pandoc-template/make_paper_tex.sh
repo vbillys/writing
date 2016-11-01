85323d2ae36859973f42036b0b69738499b0e71e
billy <billy@ciirobot.com>
2016-11-01 11:50:15 +0800

    fix bash scripts to make paper using pandoc and latex

--- ieee-pandoc-template/make_paper_tex.sh
+++ ieee-pandoc-template/make_paper_tex.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#! /bin/bash
 pandoc \
 	--standalone \
 	--natbib \
@@ -8,3 +8,4 @@ pandoc \
 	-H custom.latex \
 	--from=markdown \
 	metadata.yaml ./sections/main.md ./sections/other.md -o paper.tex
+#docker run --rm -it -v $(pwd):/var/texlive harshjv/texlive-2015 xelatex paper.tex
